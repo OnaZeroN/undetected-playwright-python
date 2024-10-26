c9f6bb8b9ca3cceb2d24dad98b8c3c70b4434d63
ZeroN <128647503+OnaZeroN@users.noreply.github.com>
2024-10-26 16:23:28 +0500

    Update build_patched.py

--- build_patched.py
+++ build_patched.py
@@ -9,78 +9,76 @@ from patch_check import main as patch_check_main
 
 
 def patch_driver(path: str):
-    # patch driver
     print(f'[PATCH] patching driver for "{path}"', file=sys.stderr)
 
-    def replace(path: str, old_str: str, new_str: str):
-        with open(path, "r") as f:
+    def replace_in_file(file_path: str, pattern: str, repl: str):
+        with open(file_path, "r", encoding="utf-8") as f:
             content = f.read()
-            content = content.replace(old_str, new_str)
-        with open(path, "w") as f:
-            f.write(content)
+            content_new = re.sub(pattern, repl, content, flags=re.MULTILINE | re.DOTALL)
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(content_new)
 
-    server_path = path + "/package/lib/server"
-    chromium_path = server_path + "/chromium"
+    server_path = os.path.join(path, "package", "lib", "server")
+    chromium_path = os.path.join(server_path, "chromium")
 
-    # comment out all "Runtime.enable" occurences
-    cr_devtools_path = chromium_path + "/crDevTools.js"
-    replace(cr_devtools_path, "session.send('Runtime.enable')", "/*session.send('Runtime.enable'), */")
+    cr_devtools_path = os.path.join(chromium_path, "crDevTools.js")
+    replace_in_file(cr_devtools_path, r"session\.send\('Runtime\.enable'\)", "/*$&*/")
 
-    cr_page_path = chromium_path + "/crPage.js"
-    with open(cr_page_path, "r") as f:
-        cr_page = f.read()
-        cr_page = cr_page.replace("this._client.send('Runtime.enable', {}),",
-                                  "/*this._client.send('Runtime.enable', {}),*/")
-        cr_page = cr_page.replace("session._sendMayFail('Runtime.enable');",
-                                  "/*session._sendMayFail('Runtime.enable');*/")
-    with open(cr_page_path, "w") as f:
-        f.write(cr_page)
+    cr_page_path = os.path.join(chromium_path, "crPage.js")
+    replace_in_file(cr_page_path, r"this\._client\.send\('Runtime\.enable', \{\}\),", "/*$&*/")
+    replace_in_file(cr_page_path, r"session\._sendMayFail\('Runtime\.enable'\);", "/*$&*/")
 
-    cr_sv_worker_path = chromium_path + "/crServiceWorker.js"
-    replace(cr_sv_worker_path, "session.send('Runtime.enable', {}).catch(e => {});",
-            "/*session.send('Runtime.enable', {}).catch(e => {});*/")
+    cr_sv_worker_path = os.path.join(chromium_path, "crServiceWorker.js")
+    replace_in_file(cr_sv_worker_path, r"session\.send\('Runtime\.enable', \{\}\)\.catch\(e => \{\}\);", "/*$&*/")
 
-    # patch ExecutionContext eval to still work
-    frames_path = server_path + "/frames.js"
+    frames_path = os.path.join(server_path, "frames.js")
 
-    _context_re = re.compile(r".*\s_context?\s*\(world\)\s*\{(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}")
-    _context_replacement = \
-        " async _context(world) {\n" \
-        """
-        // atm ignores world_name
-        if (this._isolatedContext == undefined) {
-          var worldName = "utility"
-          var result = await this._page._delegate._mainFrameSession._client.send('Page.createIsolatedWorld', {
+    with open(frames_path, "r", encoding="utf-8") as f:
+        frames_js = f.read()
+
+    custom_imports = """
+// undetected-undetected_playwright-patch - custom imports
+var _crExecutionContext = require('./chromium/crExecutionContext');
+var _dom = require('./dom');
+"""
+
+    if custom_imports not in frames_js:
+        frames_js = custom_imports + frames_js
+
+    context_pattern = r"async _context\(world\) \{[\s\S]*?\}"
+    new_context_function = """
+async _context(world) {
+    if (this._isolatedContexts == undefined) {
+        this._isolatedContexts = new Map();
+    }
+    let context = this._isolatedContexts.get(world);
+    if (!context) {
+        var worldName = world._name; // Используем _name, если name недоступно
+        var result = await this._page._delegate._mainFrameSession._client.send('Page.createIsolatedWorld', {
             frameId: this._id,
             grantUniveralAccess: true,
             worldName: worldName
-          });
-          var crContext = new _crExecutionContext.CRExecutionContext(this._page._delegate._mainFrameSession._client, {id:result.executionContextId})
-          this._isolatedContext = new _dom.FrameExecutionContext(crContext, this, worldName)
-        }
-        return this._isolatedContext
-        \n""" \
-        "}"
-    clear_re = re.compile(
-        r".\s_onClearLifecycle?\s*\(\)\s*\{")
-    clear_repl = \
-        " _onClearLifecycle() {\n" \
-        """
-        this._isolatedContext = undefined;
-        """
+        });
+        var crContext = new _crExecutionContext.CRExecutionContext(this._page._delegate._mainFrameSession._client, { id: result.executionContextId });
+        context = new _dom.FrameExecutionContext(crContext, this, worldName);
+        this._isolatedContexts.set(world, context);
+    }
+    return context;
+}
+"""
 
-    with open(frames_path, "r") as f:
-        frames_js = f.read()
-        frames_js = "// undetected-undetected_playwright-patch - custom imports\n" \
-                    "var _crExecutionContext = require('./chromium/crExecutionContext')\n" \
-                    "var _dom =  require('./dom')\n" \
-                    + "\n" + frames_js
+    frames_js = re.sub(context_pattern, new_context_function, frames_js, flags=re.MULTILINE | re.DOTALL)
 
-        # patch _context function
-        frames_js = _context_re.subn(_context_replacement, frames_js, count=1)[0]
-        frames_js = clear_re.subn(clear_repl, frames_js, count=1)[0]
+    clear_lifecycle_pattern = r"_onClearLifecycle\(\) \{[\s\S]*?\}"
+    new_clear_lifecycle_function = """
+_onClearLifecycle() {
+    this._isolatedContexts = new Map();
+}
+"""
 
-    with open(frames_path, "w") as f:
+    frames_js = re.sub(clear_lifecycle_pattern, new_clear_lifecycle_function, frames_js, flags=re.MULTILINE | re.DOTALL)
+
+    with open(frames_path, "w", encoding="utf-8") as f:
         f.write(frames_js)
 
 
